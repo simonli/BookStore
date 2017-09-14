40e56a4cfd2bb9c8ec8b7d2d87d235de0782a261
simonli <simonli@live.com>
2017-09-14 13:50:27 +0800

    Settings component

---
+++ BookStore/ViewComponents/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using BookStore.Domain.DAL;
+using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.ViewComponents
+{
+    public class Settings : ViewComponent
+    {
+        private readonly BookStoreContext _context;
+
+        public Settings(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int abc)
+        {
+//            var user = u ?? await _context.Users.FirstOrDefaultAsync(m =>
+//                           m.Username == HttpContext.User.Identity.Name);
+            return View();
+        }
+    }
+}
